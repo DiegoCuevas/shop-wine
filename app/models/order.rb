8e5df7190377503116e799a99ac372c5cd833a3f
Diego Cuevas <diegocuevas579@gmail.com>
2021-10-25 22:15:46 -0500

    ADD: order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,31 @@
 class Order < ApplicationRecord
+  include AASM
+  
   belongs_to :user
+  
+  aasm column: :payment_status do
+    state :unpaid, initial: true
+    state :paid
+    state :pending
+
+    event :to_pay do
+      transitions from: :not_pay, to: :paid
+    end
+    event :not_paid do
+      transitions from: :not_pay, to: :pending
+    end
+  end
+  aasm column: :order_status do
+    state :received, initial: true
+    state :in_preparation
+    state :in_delivery
+    state :delivered
+
+    event :to_pay do
+      transitions from: :not_pay, to: :paid
+    end
+    event :to_pay do
+      transitions from: :not_pay, to: :pending
+    end
+  end
 end
